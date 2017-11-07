68c52f26df2f27e1fd67acf0f8ef42e4f63e83c7
Ant2288 <1248207321@qq.com>
2017-11-07 20:11:46 +0800

    完成一个布局页面

--- src/main/java/cn/abble/jhtml/layout/BorderLayout.java
+++ src/main/java/cn/abble/jhtml/layout/BorderLayout.java
@@ -27,19 +27,37 @@ public class BorderLayout implements Layout{
     @Override
     public void init(Tag root){
         Tag east = Create.createTagByNameWithAttribute("div","id='jhtml_east'");
-        Tag west = Create.createTagByNameWithAttribute("div","id='jhtml_east'");
+        Tag west = Create.createTagByNameWithAttribute("div","id='jhtml_west'");
         Tag south = Create.createTagByNameWithAttribute("div","id='jhtml_south'");
         Tag north = Create.createTagByNameWithAttribute("div","id='jhtml_north'");
         Tag center = Create.createTagByNameWithAttribute("div","id='jhtml_center'");
-        root.addChildren(east);
-        root.addChildren(west);
-        root.addChildren(south);
-        root.addChildren(north);
-        root.addChildren(center);
+
+        Tag container = Create.createTagByNameWithAttribute("div","id='jhtml_container'");
+        container.addChildren(north);
+        Tag main = Create.createTagByNameWithAttribute("div","id='jhtml_main'");
+        container.addChildren(main);
+        Tag centerBox = Create.createTagByNameWithAttribute("div","id='jhtml_centerBox'");
+        main.addChildren(centerBox);
+        centerBox.addChildren(center);
+        main.addChildren(east);
+        main.addChildren(west);
+        container.addChildren(south);
+        root.addChildren(container);
+
+
     }
 
     @Override
     public void addCSS(CSS css) {
-        css.addManyCSS("#jhtml_east","color:red");
+        css.addManyCSS("*","padding:0;margin:0");
+        css.addManyCSS("body,html","height:100%;font:20px/40px\"microsoft yahei\";color:black");
+        css.addManyCSS("#jhtml_container","width:100%;margin:0 auto;height:100%");
+        css.addManyCSS("#jhtml_north,#jhtml_south","height:12.5%;width:100%;background:deeppink;");
+        css.addManyCSS("#jhtml_main","height:75%");
+        css.addManyCSS("#jhtml_centerBox,#jhtml_west,#jhtml_east","height:100%;float:left");
+        css.addManyCSS("#jhtml_centerBox","width:100%;color:#1fc195");
+        css.addManyCSS("#jhtml_west","background: lightgrey;width: 15%;margin-left: -100%;");
+        css.addManyCSS("#jhtml_east","background: #2e6da4;width: 15%;margin-left: -15%;");
+        css.addManyCSS("#jhtml_center","padding-left: 15%;");
     }
 }
